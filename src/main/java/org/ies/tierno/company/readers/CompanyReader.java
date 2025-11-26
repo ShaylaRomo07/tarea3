91b3bbc3b9d68ce5818bc7b333a217dd41585764
ShaylaRomo07 <sarcclases@gmail.com>
2025-11-26 13:35:11 +0100

    Readers acabados

--- src/main/java/org/ies/tierno/company/readers/CompanyReader.java
+++ src/main/java/org/ies/tierno/company/readers/CompanyReader.java
@@ -1,6 +1,7 @@
 package org.ies.tierno.company.readers;
 
 import org.ies.tierno.company.model.Company;
+import org.ies.tierno.company.model.Department;
 
 import java.util.Scanner;
 
@@ -14,6 +15,25 @@ public class CompanyReader {
     }
 
     public Company read() {
-
+        System.out.println("Introduce la informacion de la compañia");
+        System.out.println("Nombre:");
+        String name = scanner.nextLine();
+        System.out.println("CIF:");
+        String cif = scanner.nextLine();
+        System.out.println("Cuantos departamentos tiene?");
+        int numberOfDepartments = scanner.nextInt();
+        scanner.nextLine();
+        while (numberOfDepartments < 0) {
+            System.out.println("Opcion invalida.Enter para continuar....");
+            scanner.nextLine();
+            System.out.println("Cuantos departamentos tiene?");
+            numberOfDepartments = scanner.nextInt();
+            scanner.nextLine();
+        }
+        Department[] departments = new Department[numberOfDepartments];
+        for (int i = 0; i < departments.length; i++) {
+            departments[i] = departmentReader.read();
+        }
+        return new Company(name, cif, departments);
     }
 }
