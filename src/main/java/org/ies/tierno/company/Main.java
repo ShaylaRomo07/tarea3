3565b6064d564e689da256d95003d13796587fc1
ShaylaRomo07 <sarcclases@gmail.com>
2025-12-01 11:59:05 +0100

    Terminado

--- src/main/java/org/ies/tierno/company/Main.java
+++ src/main/java/org/ies/tierno/company/Main.java
@@ -8,12 +8,12 @@ import org.ies.tierno.company.readers.EmployeeReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner=new Scanner(System.in);
-        EmployeeReader employeeReader=new EmployeeReader(scanner);
-        DepartmentReader departmentReader=new DepartmentReader(scanner,employeeReader);
-        CompanyReader companyReader=new CompanyReader(scanner,departmentReader);
-        CompanyApp companyApp=new CompanyApp(scanner,companyReader);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        EmployeeReader employeeReader = new EmployeeReader(scanner);
+        DepartmentReader departmentReader = new DepartmentReader(scanner, employeeReader);
+        CompanyReader companyReader = new CompanyReader(scanner, departmentReader);
+        CompanyApp companyApp = new CompanyApp(scanner, companyReader);
         companyApp.run();
     }
 }
--- src/main/java/org/ies/tierno/company/model/Workstation.java
+++ src/main/java/org/ies/tierno/company/model/Workstation.java
@@ -1,5 +1,5 @@
 package org.ies.tierno.company.model;
 
 public enum Workstation {
-    programador,jefe_de_proyecto,disenador,no_asignado
+    programador, jefe_de_proyecto, disenador, no_asignado
 }
